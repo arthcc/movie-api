f3bfaea4ec74d9b7a2c3ff0bd9c1d24d4a0929e1
Arthur Cunha <arthurdaconceicaocunha@gmail.com>
2023-07-09 22:28:33 -0300

    Revert "Pequenas mudanças, só pra deixar bonito"
    This reverts commit 8e9d7b5b5a5d7aa9fa3416b95349b00a893bf49b.

--- src/Services/FilmeService.cs
+++ src/Services/FilmeService.cs
@@ -5,8 +5,6 @@ using FilmeApi2.Helpers;
 using SQLitePCL;
 using System;
 using System.Linq;
-using System.Net;
-using System.Web.Http;
 
 namespace FilmeApi2.Services
 {
@@ -36,55 +34,26 @@ namespace FilmeApi2.Services
             try
             {
                 var filmeResult = _context.Filmes.FirstOrDefault(x => x.Genero.Equals(genero));
-
-                if (filmeResult == null)
-                {
-                    throw new HttpResponseException(HttpStatusCode.NotFound);
-                }
                 BuscarGeneroDto filme = _mapper.Map<BuscarGeneroDto>(filmeResult);
                 return ApiResponse<BuscarGeneroDto>.Success(filme);
-
             }
-            catch (HttpResponseException ex)
+            catch (Exception ex)
             {
-               // return ApiResponse<BuscarGeneroDto>.Error(ex.Response.ReasonPhrase, 404);
-
-                if (ex.Response.StatusCode == HttpStatusCode.NotFound)
-                {
-                    return ApiResponse<BuscarGeneroDto>.Error("Genero não encontrado", 404);
-                }
-                else
-                {
-                    return ApiResponse<BuscarGeneroDto>.Error("Erro interno no servidor", 500);
-                }
+                return ApiResponse<BuscarGeneroDto>.Error(ex.Message);
             }
         }
 
-       public ApiResponse<BuscarFilmeDto> BuscarFilme(string titulo)
-        { 
+        public ApiResponse<BuscarFilmeDto> BuscarFilme(string titulo)
+        {
             try
             {   
                 var filmeResult = _context.Filmes.FirstOrDefault(x => x.Titulo.Equals(titulo));
-                if (filmeResult == null)
-                {
-                    throw new HttpResponseException(HttpStatusCode.NotFound);
-                }
-               
                 BuscarFilmeDto filme = _mapper.Map<BuscarFilmeDto>(filmeResult);
                 return ApiResponse<BuscarFilmeDto>.Success(filme);
             }
-            catch (HttpResponseException ex)
+            catch (Exception ex)
             {
-                // return ApiResponse<BuscarGeneroDto>.Error(ex.Response.ReasonPhrase, 404);
-
-                if (ex.Response.StatusCode == HttpStatusCode.NotFound)
-                {
-                    return ApiResponse<BuscarFilmeDto>.Error("Filme não encontrado", 404);
-                }
-                else
-                {
-                    return ApiResponse<BuscarFilmeDto>.Error("Erro interno no servidor", 500);
-                }
+                return ApiResponse<BuscarFilmeDto>.Error(ex.Message);
             }
         }
 
