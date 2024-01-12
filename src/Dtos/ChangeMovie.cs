987315decd6034f6635a2d4883c4b73eefb7adae
Arthur Cunha <arthurdaconceicaocunha@gmail.com>
2024-01-12 12:02:06 -0300

    :fire: removing local file

---
+++ src/Dtos/ChangeMovie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmeApi2.Dtos
+{
+    public class ChangeMovie
+    {
+        [Key]
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public int RunTime { get; set; }
+        public string Cast { get; set; }
+
+    }
+
+}
